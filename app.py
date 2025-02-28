4dd2919ac84f205ee597cb55c94405fe27fc6874
varsimashviliLuka <varsimashvili.official@gmail.com>
2025-02-28 14:16:18 +0400

    passing Config for docker to have connection with database

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from src import create_app
 from src.config import DevConfig
 
-flask_app = create_app(config=DevConfig)
+# flask_app = create_app(config=DevConfig)
 
-# flask_app = create_app()
+flask_app = create_app()
 
 if __name__ == "__main__":
     flask_app.run(debug=True, host='0.0.0.0')
