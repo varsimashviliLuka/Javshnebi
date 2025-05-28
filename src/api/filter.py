29751648ece0856e1b14bea191b7e0dd2abf212e
varsimashviliLuka <varsimashvili.official@gmail.com>
2025-05-29 00:24:17 +0400

    updating filers

--- src/api/filter.py
+++ src/api/filter.py
@@ -1,11 +1,17 @@
 from flask_restx import Resource
 from flask import request
 
-from src.api.nsmodels import filter_ns, filter_model
+from flask_jwt_extended import jwt_required
+
+from src.api.nsmodels import filter_ns, filter_model, get_data_model
 from src.extensions import Agency
+
+from src.models import DynamicTable, Category, Center
 
 @filter_ns.route('/filter')
 class FilterApi(Resource):
+    @jwt_required()
+    @filter_ns.doc(security='JsonWebToken')
     @filter_ns.expect(filter_model)
     def post(self):
         ''' მონაცემების გაფილტვრა '''
@@ -20,6 +26,28 @@ class FilterApi(Resource):
         availability = agency.check_availability()
 
         return {"bookable_slots": availability}
+    
+    @jwt_required()
+    @filter_ns.doc(security='JsonWebToken')
+    @filter_ns.marshal_with(get_data_model)
+    def get(self):
+        ''' ყველა შესაძლო ჯავშნის ნახვა '''
+
+        available_slots = []
+        data = DynamicTable.query.all()
+        for i in data:
+            result = {
+                'id': i.id,
+                'center_name_english': Center.query.filter_by(official_center_id=i.official_center_id).first().name_english,
+                'category_name_english': Category.query.filter_by(official_category_id=i.official_category_id).first().name_english,
+                'availability': i.availability
+            }
+            available_slots.append(result)
+        return available_slots, 200
+
+
+    
+
 
 
 
--- src/api/nsmodels/filter.py
+++ src/api/nsmodels/filter.py
@@ -7,3 +7,10 @@ filter_model = api.model('Filter', {
     'category_id': fields.Integer(required=True, description='კატეგორიის id', example=4),
     'center_id': fields.Integer(required=True,description='ცენტრის id', example=2)
 })
+
+get_data_model = api.model('Get_Data', {
+    'id': fields.Integer(readOnly=True, description='ID'),
+    'center_name_english': fields.String(required=True,description='სახელი ცენტრის'),
+    'category_name_english': fields.String(required=True,description='სახელი კატეგორიის'),
+    'availability': fields.Raw(description='Availability JSON data')
+})
